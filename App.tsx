b56157653dd7d47f177ad98251a64f1f72627e42
jaepanman <65473635+jaepanman@users.noreply.github.com>
2026-01-11 20:24:43 +0900

    Add files via upload

--- App.tsx
+++ App.tsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 import Home from './pages/Home';
 import Courses from './pages/Courses';
 import Locations from './pages/Locations';
@@ -11,6 +12,7 @@ import TrialForm from './pages/TrialForm';
 const App: React.FC = () => {
   return (
     <Router>
+      <ScrollToTop />
       <div className="flex flex-col min-h-screen">
         <Header />
         <main className="flex-grow">
